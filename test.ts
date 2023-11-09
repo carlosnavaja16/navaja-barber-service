858db8a71c624dd4d1c1aa49b9b63dae6256224c
Carlos Rossi <carlos.rossi@ukg.com>
2023-11-08 21:16:15 -0500

    Wed Nov  8 21:16:15 EST 2023

--- test.ts
+++ test.ts
@@ -1,10 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
-
-if (process.env.CREDENTIALS == undefined) {
-  throw new Error('Missing environment variable CREDENTIALS');
-} else {
-  const credentials = JSON.parse(process.env.CREDENTIALS);
-  console.log(`client_email: ${credentials.client_email}`);
-  console.log(`private_key: ${credentials.private_key}`);
+if (!process.env.CREDENTIALS !== undefined) {
+  const creds = JSON.parse(process.env.CREDENTIALS || '{}');
+  console.log(creds);
 }
