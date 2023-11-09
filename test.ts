ad779a3e72d91eaf3c6e99d8c1ac16b2528ec051
Carlos Rossi <carlos.rossi@ukg.com>
2023-11-08 21:58:53 -0500

    Wed Nov  8 21:58:53 EST 2023

--- test.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config();
-if (!process.env.CREDENTIALS !== undefined) {
-  const creds = JSON.parse(process.env.CREDENTIALS || '{}');
-  console.log(creds);
-}
